00072d4bc5855a2abd4cae73cb2b2314788ca6e6
builderb gamer <builderbgamer@gmail.com>
2018-08-31 00:15:36 +0100

    Added all apis

---
+++ BotListAPI/ListAPI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotListAPI
+{
+    public class ListAPI
+    {
+        public readonly string Name;
+        public readonly string Website;
+        public readonly ListOwner Owner;
+        public bool Enabled = true;
+        public ListAPI(string name, string website, ListOwner owner)
+        {
+            Name = name;
+            Website = website;
+            Owner = owner;
+        }
+    }
+    public class ListOwner
+    {
+        public ListOwner(string name, ulong id)
+        {
+            Name = name;
+            Id = id;
+        }
+        public string Name;
+        public ulong Id;
+    }
+}
---
+++ BotListAPI/ListConfig.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotListAPI
+{
+    public class TokenConfig
+    {
+        /// <summary>
+        /// Discord Bots token [bots.discord.pw]
+        /// </summary>
+        public string DiscordBots = "";
+
+        /// <summary>
+        /// Discord Bot List token [discordbots.org]
+        /// </summary>
+        public string DiscordBotList = "";
+
+        /// <summary>
+        /// Bots For Discord token [botsfordiscord.com]
+        /// </summary>
+        public string BotsForDiscord = "";
+
+        /// <summary>
+        /// Carbonitex token [carbonitex.net]
+        /// </summary>
+        public string Carbonitex = "";
+
+        /// <summary>
+        /// Discord Bot List v2 token [discordbotlist.com]
+        /// </summary>
+        public string DiscordBotListv2 = "";
+
+        /// <summary>
+        /// Botlisst Space token [botlist.space]
+        /// </summary>
+        public string BotListSpace = "";
+
+        /// <summary>
+        /// Bots On Discord token [bots.ondiscord.xyz]
+        /// </summary>
+        public string BotsOnDiscord = "";
+
+        /// <summary>
+        /// Discord Bot World token [discordbot.world]
+        /// </summary>
+        public string DiscordBotWorld = "";
+
+        /// <summary>
+        /// Discord Bots Group token [discordbots.group]
+        /// </summary>
+        public string DiscordBotsGroup = "";
+
+        /// <summary>
+        /// Discord List App token [bots.discordlist.app]
+        /// </summary>
+        public string DiscordListApp = "";
+
+        /// <summary>
+        /// Discord Services token [discord.services]
+        /// </summary>
+        public string DiscordServices = "";
+
+        /// <summary>
+        /// Divine Bot List token [divinediscordbots.com]
+        /// </summary>
+        public string DivineBotList = "";
+    }
+}
