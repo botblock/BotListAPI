1906bfa74989b2a99fdd0a0df684ee0fb15fe622
builderb gamer <builderbgamer@gmail.com>
2018-09-01 00:00:23 +0100

    Adding post method

--- BotListAPI/ListAPI.cs
+++ BotListAPI/ListAPI.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 
 namespace BotListAPI
@@ -8,13 +11,36 @@ namespace BotListAPI
     {
         public readonly string Name;
         public readonly string Website;
+        public string API;
         public readonly ListOwner Owner;
         public bool Enabled = true;
-        public ListAPI(string name, string website, ListOwner owner)
+        public HttpClient Http;
+        public ListAPI(string name, string website, string api, ListOwner owner)
         {
             Name = name;
             Website = website;
+            API = api;
             Owner = owner;
+        }
+
+        public bool Post(BaseSocketClient client, string token)
+        {
+            if (Http == null)
+            {
+                Http = new HttpClient();
+                Http.DefaultRequestHeaders.Add("Authorization", token);
+                API = API.Replace("{0}", client.CurrentUser.Id.ToString());
+            }
+            try
+            {
+                StringContent Content = new StringContent("{ \"server_count\": 0 }".Replace("0", client.Guilds.Count.ToString()), Encoding.UTF8, "application/json");
+                Http.PostAsync(API, Content);
+            }
+            catch(Exception)
+            {
+                return false;
+            }
+            return true;
         }
     }
     public class ListOwner
--- BotListAPI/ListClient.cs
+++ BotListAPI/ListClient.cs
@@ -9,6 +9,7 @@ namespace BotListAPI
         private readonly TokenConfig Config;
         private readonly BaseSocketClient Client;
         private BackgroundWorker Worker = new BackgroundWorker();
+        private bool WorkerEnabled = false;
         public ListClient(BaseSocketClient client, TokenConfig config)
         {
             Client = client;
@@ -21,7 +22,9 @@ namespace BotListAPI
         /// </summary>
         public void Start()
         {
-            Worker.RunWorkerAsync();
+            WorkerEnabled = true;
+            if (!WorkerEnabled)
+                Worker.RunWorkerAsync();
         }
 
         /// <summary>
@@ -29,7 +32,9 @@ namespace BotListAPI
         /// </summary>
         public void Stop()
         {
-            Worker.CancelAsync();
+            WorkerEnabled = false;
+            if (WorkerEnabled)
+                Worker.CancelAsync();
         }
 
         private void PostCount(object sender, DoWorkEventArgs e)
@@ -37,32 +42,32 @@ namespace BotListAPI
             while (true)
             {
                 System.Threading.Thread.Sleep(new TimeSpan(0, 5, 0));
-                Console.WriteLine("Hello");
+                DiscordBots.Post(Client, Config.DiscordBots);
             }
         }
 
-        public ListAPI DiscordBots = new ListAPI("Discord Bots", "https://bots.discord.pw", new ListOwner("meew0#9811", 66237334693085184));
+        public ListAPI DiscordBots = new ListAPI("Discord Bots", "https://bots.discord.pw", "https://discordbots.org/api/bots/{0}/stats", new ListOwner("meew0#9811", 66237334693085184));
 
-        public ListAPI DiscordBotList = new ListAPI("Discord Bot List", "https://discordbots.org", new ListOwner("Oliy#0330", 129908908096487424));
+        public ListAPI DiscordBotList = new ListAPI("Discord Bot List", "https://discordbots.org", "", new ListOwner("Oliy#0330", 129908908096487424));
 
-        public ListAPI DiscordBotListv2 = new ListAPI("Discord Bot List v2", "https://discordbotlist.com", new ListOwner("luke#0123", 149505704569339904));
+        public ListAPI DiscordBotListv2 = new ListAPI("Discord Bot List v2", "https://discordbotlist.com", "", new ListOwner("luke#0123", 149505704569339904));
 
-        public ListAPI BotsForDiscord = new ListAPI("Bots For Discord", "https://botsfordiscord.com", new ListOwner("Habchy#1665", 162780049869635584));
+        public ListAPI BotsForDiscord = new ListAPI("Bots For Discord", "https://botsfordiscord.com", "", new ListOwner("Habchy#1665", 162780049869635584));
 
-        public ListAPI Carbonitex = new ListAPI("Carbonitex", "https://www.carbonitex.net", new ListOwner("jet#9999", 228290260239515649));
+        public ListAPI Carbonitex = new ListAPI("Carbonitex", "https://www.carbonitex.net", "", new ListOwner("jet#9999", 228290260239515649));
 
-        public ListAPI BotListSpace = new ListAPI("Bot List Space", "https://botlist.space", new ListOwner("PassTheMayo#1281", 153173425844781056));
+        public ListAPI BotListSpace = new ListAPI("Bot List Space", "https://botlist.space", "", new ListOwner("PassTheMayo#1281", 153173425844781056));
 
-        public ListAPI BotsOnDiscord = new ListAPI("Bots On Discord", "https://bots.ondiscord.xyz", new ListOwner("Brussell#0660", 95286900801146880));
+        public ListAPI BotsOnDiscord = new ListAPI("Bots On Discord", "https://bots.ondiscord.xyz", "", new ListOwner("Brussell#0660", 95286900801146880));
 
-        public ListAPI DiscordBotWorld = new ListAPI("Discord Bot World", "https://discordbot.world", new ListOwner("Tetrabyte#0001", 156114103033790464));
+        public ListAPI DiscordBotWorld = new ListAPI("Discord Bot World", "https://discordbot.world", "", new ListOwner("Tetrabyte#0001", 156114103033790464));
 
-        public ListAPI DiscordBotsGroup = new ListAPI("Discord Bots Group", "https://discordbots.group", new ListOwner("DetectiveHuman#0767", 423220263161692161));
+        public ListAPI DiscordBotsGroup = new ListAPI("Discord Bots Group", "https://discordbots.group", "", new ListOwner("DetectiveHuman#0767", 423220263161692161));
 
-        public ListAPI DiscordListApp = new ListAPI("Discord List App", "https://bots.discordlist.app", new ListOwner("Auxim#0001", 66166172835385344));
+        public ListAPI DiscordListApp = new ListAPI("Discord List App", "https://bots.discordlist.app", "", new ListOwner("Auxim#0001", 66166172835385344));
 
-        public ListAPI DiscordServices = new ListAPI("Discord Services", "http://discord.services", new ListOwner("Tails#0420", 472573259108319237));
+        public ListAPI DiscordServices = new ListAPI("Discord Services", "http://discord.services", "", new ListOwner("Tails#0420", 472573259108319237));
 
-        public ListAPI DivineBotList = new ListAPI("Divine Bot List", "https://divinediscordbots.com", new ListOwner("Sworder#1234", 240508683455299584));
+        public ListAPI DivineBotList = new ListAPI("Divine Bot List", "https://divinediscordbots.com", "", new ListOwner("Sworder#1234", 240508683455299584));
     }
 }
